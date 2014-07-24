53bd63932576a21e5d22cc85bc76ff04fbc4aac3
Shirleen Kneppers <shirleenkneppers@shirleens-mini.home>
2014-07-24 12:23:48 +0100

    SQLite Application
    This application was part of my Dissertation. It was to see if the same
    applications can be developed on tree different mobile operating
    systems.

--- TUCMP300SQLite/ViewController.h
+++ TUCMP300SQLite/ViewController.h
@@ -7,7 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @interface ViewController : UIViewController
+
+@property (strong, nonatomic) NSString *databasePath;
+@property (nonatomic) sqlite3 *contactDB;
+
+@property (strong, nonatomic) IBOutlet UITextField *name;
+@property (strong, nonatomic) IBOutlet UITextField *address;
+@property (strong, nonatomic) IBOutlet UITextField *phone;
+@property (strong, nonatomic) IBOutlet UILabel *status;
+
+- (IBAction)saveData:(id)sender;
+- (IBAction)findContact:(id)sender;
+
 
 @end
